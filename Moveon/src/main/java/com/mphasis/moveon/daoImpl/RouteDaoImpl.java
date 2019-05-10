702766ea5fd71efa579220d6e5edb47984758de5
sangeeta6297 <49521701+sangeeta6297@users.noreply.github.com>
2019-05-10 11:24:00 +0530

    Add files via upload
    vehicle and route

---
+++ Moveon/src/main/java/com/mphasis/moveon/daoImpl/RouteDaoImpl.java
@@ -0,0 +1,77 @@
+package com.mphasis.moveon.daoImpl;
+
+import java.util.List;
+
+
+
+
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import com.mphasis.moveon.daos.RouteDao;
+import com.mphasis.moveon.entities.Route;
+import com.mphasis.moveon.entities.Vehicle;
+@Repository
+
+public class RouteDaoImpl implements RouteDao {
+	@Autowired
+		SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+			this.sessionFactory = sessionFactory;
+		}
+
+	public void insertRoute(Route route) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		session.save(route);
+		tr.commit();
+
+		
+
+	}
+
+	public void updateRoute(Route route) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		session.update(route);
+		tr.commit();
+		session.close();
+
+	}
+
+	public void deleteRoute(int route_Id) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		Route route=session.get(Route.class,route_Id);
+		session.delete(route);
+		tr.commit();
+
+	}
+
+	public List<Route> getAll() {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		List<Route> routes=session.createCriteria(Route.class).list();
+		return routes;
+	}
+
+	public Vehicle getAllBySourceAndDestination(String source, String destination) {
+		Session session = sessionFactory.openSession();
+		Transaction tr = session.beginTransaction();
+		Query query1=session.createSQLQuery("select vehicle_Id,vehicle_Name,vehicle_Type,seat_Capacity,vehicle_Num,fare where vehicle_Id = (select vehicle_Id from schedule where route_id = (select route_id from route where source=:source and destination=:destination");
+		query1.setParameter("source", source);
+		query1.setParameter("destination", destination);
+        Vehicle vehicle=(Vehicle) query1.uniqueResult();
+        tr.commit();
+		return vehicle;
+	}
+
+}
---
+++ Moveon/src/main/java/com/mphasis/moveon/daoImpl/VehicleDaoImpl.java
@@ -0,0 +1,73 @@
+package com.mphasis.moveon.daoImpl;
+
+import java.util.List;
+
+
+
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mphasis.moveon.daos.VehicleDao;
+
+import com.mphasis.moveon.entities.Vehicle;
+
+
+@Repository
+
+public class VehicleDaoImpl implements VehicleDao {
+	@Autowired
+	SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public void insertVehicle(Vehicle vehicle) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		session.save(vehicle);
+		tr.commit();
+
+	}
+
+	public void updateVehicle(Vehicle vehicle) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		session.update(vehicle);
+		tr.commit();
+		session.close();
+
+	}
+
+	public void deleteVehicle(int vehicle_Id) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		Vehicle vehicle=session.get(Vehicle.class,vehicle_Id);
+		session.delete(vehicle);
+		tr.commit();
+
+	}
+
+	public List<Vehicle> getAll() {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		List<Vehicle> vehicles=session.createCriteria(Vehicle.class).list();
+		return vehicles;
+	}
+
+	 public List<Vehicle> getAllByType(String vehicle_Type) {
+		 Session session=sessionFactory.openSession();
+			Transaction tr=session.beginTransaction();
+			Criteria cri=session.createCriteria(Vehicle.class);
+			cri.add(Restrictions.eq("vehicle_Type",vehicle_Type ));
+			List<Vehicle> vehicle=cri.list();
+			tr.commit();
+			return vehicle;
+	 }
+}
