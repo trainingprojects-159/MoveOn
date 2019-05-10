01b4c40f2d49cf9a1619056533c110da2f8aeb77
bindu.kona <bindu.kona@WKSBAN24TR17030.corp.mphasis.com>
2019-05-10 11:13:03 +0530

    contollers are created

--- Moveon/src/main/java/com/mphasis/moveon/controllers/CustomerController.java
+++ Moveon/src/main/java/com/mphasis/moveon/controllers/CustomerController.java
@@ -1,13 +1,10 @@
 package com.mphasis.moveon.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin")
-public class AdminControllers {
+@RequestMapping("/customer")
+public class CustomerController {
 
-	
-	
 }
