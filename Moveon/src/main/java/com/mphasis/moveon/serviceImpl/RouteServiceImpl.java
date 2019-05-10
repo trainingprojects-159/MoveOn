e6e1059a338da932aaec62e6de9a0cb9a42ac955
sangeeta6297 <49521701+sangeeta6297@users.noreply.github.com>
2019-05-10 11:25:41 +0530

    Add files via upload
    service route and vehicle implementation

---
+++ Moveon/src/main/java/com/mphasis/moveon/serviceImpl/RouteServiceImpl.java
@@ -0,0 +1,46 @@
+package com.mphasis.moveon.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mphasis.moveon.daos.RouteDao;
+import com.mphasis.moveon.entities.Route;
+import com.mphasis.moveon.entities.Vehicle;
+import com.mphasis.moveon.service.RouteService;
+@Service
+public class RouteServiceImpl implements RouteService {
+
+	@Autowired
+	RouteDao routeDao;
+	public void setRouteDao(RouteDao routeDao) {
+		this.routeDao = routeDao;
+	}
+
+	public void addRoute(Route route) {
+		System.out.println("route values in service"+route.getDistance());
+		routeDao.insertRoute(route);
+
+	}
+
+	public void editRoute(Route route) {
+	routeDao.updateRoute(route);
+
+	}
+
+	public void removeRoute(int route_Id) {
+		routeDao.deleteRoute(route_Id);
+
+
+	}
+
+	public List<Route> getAll() {
+		return  routeDao.getAll();
+	}
+
+	public List<Vehicle> getAllBySourceAndDestination(String source, String destination) {
+		return routeDao. getAllBySourceAndDestination(source,destination);
+	}
+
+}
---
+++ Moveon/src/main/java/com/mphasis/moveon/serviceImpl/VehicleServiceImpl.java
@@ -0,0 +1,44 @@
+package com.mphasis.moveon.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mphasis.moveon.daos.VehicleDao;
+import com.mphasis.moveon.entities.Vehicle;
+import com.mphasis.moveon.service.VehicleService;
+@Service
+public class VehicleServiceImpl implements VehicleService {
+		
+	@Autowired
+	public VehicleDao vehicleDao;
+	public void setVehicleDao(VehicleDao vehicleDao) {
+		this.vehicleDao = vehicleDao;
+	}
+
+	public void addVehicle(Vehicle vehicle) {
+		System.out.println("vehicle values in service"+vehicle.getVehicle_Name());
+		vehicleDao.insertVehicle(vehicle);
+
+	}
+
+	public void editVehicle(Vehicle vehicle) {
+		vehicleDao.updateVehicle(vehicle);
+
+	}
+
+	public void removeVehicle(int vehicle_Id) {
+		vehicleDao.deleteVehicle(vehicle_Id);
+
+	}
+
+	public List<Vehicle> getAll() {
+		return  vehicleDao.getAll();
+	}
+
+	public List<Vehicle> getAllByType(String vehicle_Type) {
+		return vehicleDao.getAllByType(vehicle_Type);
+	}
+
+}
