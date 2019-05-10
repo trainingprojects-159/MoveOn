5b439b7eb2a6a4fea1a0c0cccd07d601ce5d8eb6
sangeeta6297 <49521701+sangeeta6297@users.noreply.github.com>
2019-05-10 11:24:49 +0530

    Add files via upload
    service route and vehicle

---
+++ Moveon/src/main/java/com/mphasis/moveon/service/RouteService.java
@@ -0,0 +1,15 @@
+package com.mphasis.moveon.service;
+
+import java.util.List;
+
+import com.mphasis.moveon.entities.Route;
+import com.mphasis.moveon.entities.Vehicle;
+
+public interface RouteService {
+	public void addRoute(Route route);
+	public void editRoute(Route route);
+	public void removeRoute(int route_Id);
+    public List<Route> getAll();
+    public List<Vehicle> getAllBySourceAndDestination(String source,String destination);
+
+}
---
+++ Moveon/src/main/java/com/mphasis/moveon/service/VehicleService.java
@@ -0,0 +1,13 @@
+package com.mphasis.moveon.service;
+
+import java.util.List;
+
+import com.mphasis.moveon.entities.Vehicle;
+
+public interface VehicleService {
+	 public void addVehicle(Vehicle vehicle);
+	    public void editVehicle(Vehicle vehicle);
+	    public void removeVehicle(int vehicle_Id);
+	    public List<Vehicle> getAll();
+	    public List<Vehicle> getAllByType(String vehicle_Type);
+}
